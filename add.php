6e7e0318b19e075c9039e003f5ee149383d25985
Jakob Onesseit <jakob.onesseit@zarm.uni-bremen.de>
2019-03-06 13:44:35 +0100

    Úpdate 1.0.0

---
+++ add.php
@@ -0,0 +1,60 @@
+<?php
+include 'headpage.php';
+
+$servername = "5.189.128.204";
+$username = "speise";
+$password = "FA18B";
+$dbname = "speisekarte";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT * FROM kategorien";
+$kategorien = $conn->query($sql);
+?>
+<div style="text-align: center; vertical-align: middle; background-color: lightblue; margin-left: 30%;margin-right: 30%">
+<h1>Insert Form</h1>
+    <div >
+
+    </div>
+        <form method="post">
+        <br></br>Kategorie: <select name="Kategorie">
+            <option value="">Kategorie wählen</option>
+            <?php while ($row = $kategorien->fetch_assoc()) { ?>
+                <option value="<?php echo $row["id"] ?>"><?php echo $row["kategorie"] ?></option>
+                <?php } ?>
+        </select>
+        <br></br>Gericht: <input type="text" name="GerichtName">
+        <p>Beschreibung: </p><textarea name="GerichtDesc" style="width: 300px; height: 100px"></textarea>
+        <br><br>kleiner Preis:<input type="text" name="smallPreis">
+        <br><br>großer Preis:<input type="text" name="largePreis">
+        <br><input type="submit" name="save">
+    </form>
+</div>
+<?php
+if($_POST["Kategorie"]!=""&&$_POST["GerichtName"]!=""&&$_POST["GerichtDesc"]!=""&&($_POST["smallPreis"]!=""||$_POST["largePreis"]!=""))
+{
+    if(isset($_POST['save'])){
+        $sql = "INSERT INTO produkte (name, beschreibung, kategorie, small_preis, large_preis) VALUES ('".$_POST["GerichtName"]."','".$_POST["GerichtDesc"]."','".$_POST["Kategorie"]."','".$_POST["smallPreis"]."','".$_POST["largePreis"]."')";
+        $result = mysqli_query($conn,$sql);
+    }
+}else if($_POST["Kategorie"]==""||$_POST["GerichtName"]==""||$_POST["GerichtDesc"]==""||($_POST["smallPreis"]==""&&$_POST["largePreis"]==""))
+{
+    $ErrorMessage="";
+    if($_POST["Kategorie"]=="")
+    {
+        $ErrorMessage=$ErrorMessage."Kategorie auswählen!\n";
+    }
+    if($_POST["GerichtName"]=="")
+    {
+        $ErrorMessage=$ErrorMessage."Essen benötigt einen Namen!\n";
+    }
+    if($_POST["GerichtDesc"]=="")
+    {
+        $ErrorMessage=$ErrorMessage."Beschreiben sie das Essen!\n";
+    }
+    if($_POST["smallPreis"]==""&&$_POST["smallPreis"]=="")
+    {
+        $ErrorMessage=$ErrorMessage."Bitte mindestens einen Preis angeben!\n";
+    }
+    echo $ErrorMessage;
+
+}
--- headpage.php
+++ headpage.php
@@ -5,5 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Titel der Seite | Name der Website</title>
     <link rel="stylesheet" href="styles.css">
+
+
+    <script>
+        var test=0;
+        function myFunction1() {
+            hide()
+            if (test==0)
+                test=3
+            else
+                test=test-1
+            unHide()
+
+        }
+        function myFunction2() {
+            hide()
+            if (test==3)
+                test=0
+            else
+                test=test+1
+            unHide()
+        }
+        function unHide() {
+            document.getElementById("kat"+test).style.display = "block";
+        }
+        function hide() {
+            document.getElementById("kat"+test).style.display = "none";
+        }
+
+    </script>
+
+
 </head>
 <body>
